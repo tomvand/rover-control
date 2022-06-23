6c50cdf0e1ebd5ebe7bc972e47939cf9c7318d7e
Tom van Dijk <tomvand@users.noreply.github.com>
2022-06-23 15:41:42 +0200

    Fix typo

--- pi/rover-control/rover_control/logfile.py
+++ pi/rover-control/rover_control/logfile.py
@@ -1,6 +1,6 @@
 class LogFile(object):
     def __init__(self, filename):
-        self.f = open(filename, 'rt')
+        self.f = open(filename, 'wt')
 
     def __del__(self):
         self.f.close()
