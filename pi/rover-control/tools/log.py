b140f0a13a74cb4a517b586be3504a667dd2b961
Tom van Dijk <tomvand@users.noreply.github.com>
2022-06-27 18:08:31 +0200

    Add log plotting functions

--- pi/rover-control/tools/log.py
+++ pi/rover-control/tools/log.py
@@ -26,9 +26,69 @@ def log_to_signals(f):
     return log
 
 
+try:
+    import matplotlib.pyplot as plt
+    import numpy as np
+except ImportError:
+    pass
+else:
+    def plot_colored(x, y, c, cmap=plt.cm.get_cmap('jet'), steps=10):
+        # https://stackoverflow.com/a/54315981
+        c = np.asarray(c)
+        c -= np.min(c)
+        c /= np.max(c)
+        c2 = c  # Not sure why python needs this...
+        it = 0
+        while it < c2.size - steps:
+            x_segm = x[it:it + steps + 1]
+            y_segm = y[it:it + steps + 1]
+            c_segm = cmap(c[it + steps // 2])
+            plt.plot(x_segm, y_segm, c=c_segm)
+            it += steps
+
+    def plot_ins_pos(log):
+        # plt.plot(
+        #     log['VISUALHOMING_STATE']['ins_e'],
+        #     log['VISUALHOMING_STATE']['ins_n'],
+        #     '-'
+        # )
+        plot_colored(
+            log['VISUALHOMING_STATE']['ins_e'],
+            log['VISUALHOMING_STATE']['ins_n'],
+            log['VISUALHOMING_STATE']['_time']
+        )
+        plt.axis('equal')
+        plt.grid(True)
+
+    def plot_ins_time(log):
+        plt.subplot(3, 1, 1)
+        plt.plot(log['VISUALHOMING_STATE']['_time'], log['VISUALHOMING_STATE']['ins_e'])
+        plt.ylabel('E [m]')
+        plt.subplot(3, 1, 2)
+        plt.plot(log['VISUALHOMING_STATE']['_time'], log['VISUALHOMING_STATE']['ins_n'])
+        plt.ylabel('N [m]')
+        plt.subplot(3, 1, 3)
+        plt.plot(log['VISUALHOMING_STATE']['_time'], np.unwrap(log['VISUALHOMING_STATE']['psi']))
+        plt.ylabel('Psi [rad]')
+
+    def plot_vectors(log):
+        for i in range(len(log['VISUALHOMING']['_time'])):
+            x = log['VISUALHOMING']['from_e'][i]
+            y = log['VISUALHOMING']['from_n'][i]
+            dx = log['VISUALHOMING']['to_e'][i] - x
+            dy = log['VISUALHOMING']['to_n'][i] - y
+            color = 'red' if log['VISUALHOMING']['source'][i] == 1 else 'blue'
+            plt.arrow(x, y, dx, dy, color=color,
+                      width=1e-4,
+                      alpha=0.3,
+                      length_includes_head=True,
+                      head_width=0.05)
+        plt.axis('equal')
+        plt.grid(True)
+
+
 if __name__ == '__main__':
     import argparse
-    import matplotlib.pyplot as plt
 
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
@@ -39,13 +99,11 @@ if __name__ == '__main__':
 
     print(json.dumps(log, indent=2))
 
+    plt.figure()
+    plot_vectors(log)
+    plot_ins_pos(log)
 
     plt.figure()
-    plt.plot(
-        log['VISUALHOMING_STATE']['ins_e'],
-        log['VISUALHOMING_STATE']['ins_n'],
-        '-'
-    )
-    plt.axis('equal')
-    plt.grid(True)
+    plot_ins_time(log)
+
     plt.show()
